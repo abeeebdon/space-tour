6c9b5eb263a403af8f8d3c3c35ab70639eedd860
Abeeb <abeebdon@gmail.com>
2024-01-16 09:29:15 +0100

    there are still changes on technology page

--- src/data.js
+++ src/data.js
@@ -74,3 +74,27 @@ export const crew = [
       ' Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. ',
   },
 ]
+
+export const technology = [
+  {
+    id: 1,
+    image: './img4.png',
+    name: 'Launch Vehicle',
+    details:
+      'A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it is quite an awe-inspiring sight on the launch pad!',
+  },
+  {
+    id: 2,
+    image: './img4.png',
+    name: 'spaceport',
+    details:
+      'A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.',
+  },
+  {
+    id: 3,
+    image: './img4.png',
+    name: 'space capsule',
+    details:
+      'A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earths atmosphere without wings. Our capsule is where you will spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.',
+  },
+]
--- src/pages/technology/Index.jsx
+++ src/pages/technology/Index.jsx
@@ -1,7 +1,47 @@
+import { useState } from 'react'
+import { technology } from '../../data'
+import { useEffect } from 'react'
+
 const Index = () => {
+  const [tech, setTech] = useState('')
+  const [index, setIndex] = useState(0)
+  useEffect(() => {
+    const techs = technology[index]
+    setTech(techs)
+  }, [])
+  const { id, name, details, image } = tech
+
+  const handleTech = (id) => {
+    technology.find((techie) => {
+      if (techie.id === id) {
+        return setTech(techie)
+      }
+    })
+  }
   return (
     <section className="technology">
-      <div>Technology</div>
+      <div className="head">
+        <p>03</p>
+        <h3>Space Launch 101</h3>
+      </div>
+      <div className="container">
+        <article key={id}>
+          <div className="image">
+            <img src={image} alt="img" />
+          </div>
+          <div className="btn">
+            {technology.map((tech) => {
+              const { id } = tech
+              return <button onClick={() => handleTech(id)}>{id}</button>
+            })}
+          </div>
+          <div className="text">
+            <h4>role</h4>
+            <h2>{name}</h2>
+            <p>{details}</p>
+          </div>
+        </article>
+      </div>
     </section>
   )
 }
