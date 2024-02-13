7a9192bebd25818a578e4a966a695539dfb027b0
Abeeb <abeebdon@gmail.com>
2024-02-14 00:44:56 +0100

    Deploy issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import Header from './components/Header'
 import Home from './pages/home/Index'
+import Header from './components/Header'
 import Destination from './pages/destination/Index'
 import Crew from './pages/crew/Index'
 import Technology from './pages/technology/Index'
